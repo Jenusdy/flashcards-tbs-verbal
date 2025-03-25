5f132c620d4a789179a7e94ff934ed661c8608f3
Fawcet Jenusdy Makay <jenusdy@gmail.com>
2025-03-25 20:05:36 +0700

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/flashcards-tbs-verbal/', // Harus sesuai dengan repo GitHub Pages
+  base: '/flashcards-tbs-verbal/', 
 })
